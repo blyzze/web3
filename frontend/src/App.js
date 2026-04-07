9ae2d1ed08d2ba017b8b0fe63c87e95f3ff63309
Gurkan Sutcu <103205814+blyzze@users.noreply.github.com>
2026-04-07 13:45:31 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import "@/App.css";
+import "./App.css";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { LanguageProvider } from "./context/LanguageContext";
 import Navbar from "./components/Navbar";
